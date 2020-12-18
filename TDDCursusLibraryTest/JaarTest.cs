51387cebf82cb714db6e5cd3b876517246cb0888
ROLAND VANDOREN <ROLAND.VANDOREN@vdabcampus.be>
2020-12-18 11:11:08 +0100

    korterSchrijven

--- TDDCursusLibraryTest/JaarTest.cs
+++ TDDCursusLibraryTest/JaarTest.cs
@@ -9,25 +9,25 @@ namespace TDDCursusLibraryTest
         [TestMethod]
         public void EenJaarDeelbaarDoor400IsEenSchrikkeljaar()
         {
-            Assert.AreEqual(true, new Jaar(2000).IsSchrikkeljaar);
+            Assert.IsTrue(new Jaar(2000).IsSchrikkeljaar);
         }
 
         [TestMethod]
         public void EenJaarDeelbaarDoor100MaarNietDoor400IsGeenSchrikkeljaar()
         {
-            Assert.AreEqual(false, new Jaar(1900).IsSchrikkeljaar);
+            Assert.IsFalse(new Jaar(1900).IsSchrikkeljaar);
         }
 
         [TestMethod]
         public void EenJaarDeelbaarDoor4IsEenSchrikkeljaar()
         {
-            Assert.AreEqual(true, new Jaar(2012).IsSchrikkeljaar);
+            Assert.IsTrue(new Jaar(2012).IsSchrikkeljaar);
         }
 
         [TestMethod]
         public void EenJaarNietDeelbaarDoor400IsGeenSchrikkeljaar()
         {
-            Assert.AreEqual(false, new Jaar(2015).IsSchrikkeljaar);
+            Assert.IsFalse(new Jaar(2015).IsSchrikkeljaar);
         }
     }
 }
