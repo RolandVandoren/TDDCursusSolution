684d2f593971764ae6435b4cd0b7ab661bed438e
ROLAND VANDOREN <ROLAND.VANDOREN@vdabcampus.be>
2020-12-22 11:47:46 +0100

    EersteImplementatieStatistiek

--- TDDCursusLibrary/Statistiek.cs
+++ TDDCursusLibrary/Statistiek.cs
@@ -8,7 +8,21 @@ namespace TDDCursusLibrary
     {
         public static decimal Gemiddelde(decimal[] getallen)
         {
-            throw new NotImplementedException();
+            if(getallen == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if(getallen.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            var totaal = decimal.Zero;
+
+            foreach (var getal in getallen)
+            {
+                totaal += getal;
+            }
+            return totaal / getallen.Length;
         }
     }
 }
